bfaa0bd43d53541fca8885d0512060ea3644129a
Cesar Goyzueta <cgoyzueta@pucp.pe>
2022-02-06 09:49:52 -0500

    update

--- server.R
+++ server.R
@@ -12,6 +12,80 @@ library("shinythemes")
 library("shinyWidgets")
 library("fresh")
 
+library("httr")
+library("readxl")
+library("leaflet")
+library("dplyr")
+library("ggplot2")
+library("htmltools")
+library("lubridate")
+library("scales")
+library("forcats")
+library("shinydashboard")
+library("shinythemes")
+library("shinyWidgets")
+library("fresh")
+GET("https://query.data.world/s/rjngr5uzhv4ihxvhak3bvny66uoj47", write_disk(tf1 <- tempfile(fileext = ".xlsx")))
+
+#Sampling
+df1 <- read_excel(tf1)
+data<-df1
+data_actuals<-data%>%slice_sample(prop=0.04)
+
+
+#111D4A azul  #639FAB  celeste   #92140C rojo   #FBF5F3 medio blanco
+df<-data_actuals%>%rename(Power='Power(MW)',site_name='Location Site Name')%>%mutate(month=month(Date),year=year(Date))%>%filter(year==2020)%>%
+    select(-year,-Reading)
+
+df$site_name <- sub(" ", "_", df$site_name)
+df$PV <- sub(" ", "_", df$PV)
+
+df$month_name<- ifelse(df$month==1,"Jan",
+                       ifelse(df$month==2,"Feb",
+                              ifelse(df$month==3,"Mar",
+                                     ifelse(df$month==4,"Apr",
+                                            ifelse(df$month==5,"May",
+                                                   ifelse(df$month==6,"Jun",
+                                                          ifelse(df$month==7,"Jul",
+                                                                 ifelse(df$month==8,"Aug",
+                                                                        ifelse(df$month==9,"Sept",
+                                                                               ifelse(df$month==10,"Oct",
+                                                                                      ifelse(df$month==11,"Nov",
+                                                                                             ifelse(df$month==12,"Dic","check"
+                                                                                             ))))))))))))
+
+df$month_name<- factor(df$month_name,levels = c("Jan","Feb","Mar","Apr","May","Jun","Jul","Aug","Sept","Oct","Nov","Dic"))
+
+
+
+# Characters to factors
+
+#reorder a factor 5MW
+df<-df%>%mutate(Capacity=fct_relevel(Capacity,"5MW","12MW","13MW"))
+
+
+#Choices in filters
+
+pv_choices<-df%>%pull(PV)%>%unique()
+months_choices<-c("Jan", "Feb","Mar", "Apr", "May" ,"Jun","Jul","Aug","Sept", "Oct","Nov","Dic")
+
+
+
+
+
+df[sapply(df, is.character)] <- lapply(df[sapply(df, is.character)],  as.factor)
+
+#theme
+mytheme <- create_theme(
+    adminlte_color(
+        light_blue = "#111D4A"
+    ),
+    adminlte_sidebar(
+        dark_hover_bg  = "#111D4A",
+        dark_color= "#111D4A"
+    )
+)
+
 # Define server logic required to draw a histogram
 server <- function(input, output) {
     
