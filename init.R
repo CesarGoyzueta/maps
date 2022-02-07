9fd489170f8afa33c17824f76964d6da3f85f27a
Cesar Goyzueta <cgoyzueta@pucp.pe>
2022-02-07 08:12:07 -0500

    a

--- init.R
+++ init.R
@@ -1,7 +1,7 @@
 #init.R
 
 my_packages=c("httr","readxl","leaflet","shiny","shinydashboard","shinythemes","shinyWidgets","highcharter","bslib",
-              "thematic","dplyr","ggplot2","htmltools","lubridate","scales","forcats","fresh")
+              "thematic","dplyr","ggplot2","htmltools","lubridate","scales","forcats","fresh","libgeos")
 
 install_if_missing=function(p){
   if(p %in% rownames(installed.packages())==FALSE){
